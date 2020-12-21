aedff0cb3aa281e6c160f5c7bed813299d0c8fbc
Piotr Glodek <pioglodek@gmail.com>
2020-12-21 12:16:34 +0100

    Add page fade  animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { Button, TodoList } from './components';
+import { motion } from 'framer-motion';
+
 function App() {
   return (
-    <>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{ duration: 0.6 }}
+    >
       <h1>Framer motion</h1>
       <p>Button</p>
       <Button label='Click me' />
       <p>Todolist with delete item animation</p>
       <TodoList />
-    </>
+    </motion.div>
   );
 }
 
